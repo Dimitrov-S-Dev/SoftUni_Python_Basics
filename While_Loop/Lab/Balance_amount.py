4806b13f8f1a6ec553645e1e1512703ab563925b
Stan <dimitrov.s.dev@gmail.com>
2023-01-07 04:15:23 +0200

    While_Loop_Lab

---
+++ While_Loop/Lab/Balance_amount.py
@@ -0,0 +1,14 @@
+command = input()
+amount = 0
+
+while command != "NoMoreMoney":
+    current_amount = float(command)
+    if current_amount < 0:
+        print(f"Invalid operation!")
+        break
+    else:
+        amount += current_amount
+        print(f"Increase: {current_amount}")
+    command = input()
+
+print(f"{amount:.2f}")
---
+++ While_Loop/Lab/Sum_of_numbers.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+sum_numbers = 0
+
+while sum_numbers < n:
+    number = int(input())
+    sum_numbers += number
+
+print(n)
