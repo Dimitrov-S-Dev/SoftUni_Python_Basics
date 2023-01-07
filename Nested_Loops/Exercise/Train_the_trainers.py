3579920d3eacee749b98bb9334c98ea61753a635
Stan <dimitrov.s.dev@gmail.com>
2023-01-08 00:09:10 +0200

    Nested_Loops_Exercise

---
+++ Nested_Loops/Exercise/Train_the_trainers.py
@@ -0,0 +1,22 @@
+jury_count = int(input())
+command = input()
+total_avr_present = 0
+presentations_count = 0
+
+while command != "Finish":
+    presentation = command
+    presentations_count += 1
+    presentation_points = 0
+
+    for points in range(jury_count):
+        current_points = float(input())
+        presentation_points += current_points
+
+    avr_present_points = presentation_points / jury_count
+    total_avr_present += avr_present_points
+
+    print(f"{presentation}- {avr_present_points:.2f}")
+
+    command = input()
+total_aver = total_avr_present / presentations_count
+print(f"Student's final assessment is {total_aver:.2f}")
