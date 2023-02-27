fcf4187bf48997bc64ebe6cb76e1929104454c08
Stan <dimitrov.s.dev@gmail.com>
2023-02-27 20:55:10 +0200

    Python_Basics

--- For_Loop/Lab/Lab.py
+++ For_Loop/Lab/Lab.py
@@ -47,4 +47,77 @@ for char in text:
 
 print(result)
 
-# Task 7 
+# Task 7 Sum of Numbers
+
+n = int(input())
+result = 0
+
+for _ in range(n):
+    current_num = int(input())
+    result += current_num
+
+print(result)
+
+# Task 8 Set of Integers
+
+import sys
+
+max_num = -sys.maxsize
+min_num = sys.maxsize
+
+n = int(input())
+
+for _ in range(n):
+    current_num = int(input())
+    if current_num > max_num:
+        max_num = current_num
+    if current_num < min_num:
+        min_num = current_num
+
+print(f"Max number: {max_num}")
+print(f"Min number: {min_num}")
+
+# Task 9 Left Right Sum
+
+n = int(input())
+left_part = 0
+
+for left in range(n):
+    current_num = int(input())
+    left_part += current_num
+
+right_part = 0
+
+for right in range(n):
+    current_num = int(input())
+    right_part += current_num
+
+
+if left_part == right_part:
+    print(f"Yes, sum = {left_part}")
+else:
+    diff = abs(left_part - right_part)
+    print(f"No, diff = {diff}")
+
+# Task 10 Even Odd Sum
+
+n = int(input())
+even_sum = 0
+odd_sum = 0
+
+for num in range(1, n + 1):
+    current_num = int(input())
+
+    if num % 2 == 0:
+        even_sum += current_num
+    else:
+        odd_sum += current_num
+
+if even_sum == odd_sum:
+    print(f"Yes")
+    print(f"Sum = {even_sum}")
+else:
+    diff = abs(even_sum - odd_sum)
+    print(f"No")
+    print(f"Diff = {diff}")
+    
--- For_Loop/Lab/Sum_numbers.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-result = 0
-
-
-for _ in range(n):
-    current_num = int(input())
-    result += current_num
-
-print(result)
