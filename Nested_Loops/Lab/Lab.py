c7de10a4ebc9383a17421a884c8b8ed2713ba9bc
Stan <dimitrov.s.dev@gmail.com>
2023-02-28 04:49:07 +0200

    Python_Basics

---
+++ Nested_Loops/Lab/Lab.py
@@ -0,0 +1,13 @@
+# Task 1 Clock
+
+for hours in range(24):
+    for minutes in range(60):
+        print(f"{hours}:{minutes}")
+
+# Task 2 Multiplication Table
+
+for first in range(1, 11):
+    for second in range(1, 11):
+        total = first * second
+        print(f"{first} * {second} = {total}")
+
