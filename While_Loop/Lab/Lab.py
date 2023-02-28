a68cde8428b46806431191597313923f8ca4f420
Stan <dimitrov.s.dev@gmail.com>
2023-02-28 05:11:52 +0200

    Python_Basics

--- While_Loop/Lab/Lab.py
+++ While_Loop/Lab/Lab.py
@@ -17,3 +17,23 @@ while command != password:
 
 print(f"Welcome {name}!")
 
+# Task 3 Sum of Numbers
+
+n = int(input())
+sum_numbers = 0
+
+while sum_numbers < n:
+    number = int(input())
+    sum_numbers += number
+
+print(n)
+
+# Task 4 Numbers 2K + 1
+
+n = int(input())
+count = 1
+
+while count <= n:
+    print(count)
+    count = (count * 2) + 1
+
--- While_Loop/Lab/Sum_of_numbers.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-sum_numbers = 0
-
-while sum_numbers < n:
-    number = int(input())
-    sum_numbers += number
-
-print(n)
