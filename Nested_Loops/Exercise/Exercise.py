68685229c7f4c2dbd60f6995181158c655d11a32
Stan <dimitrov.s.dev@gmail.com>
2023-08-13 03:57:54 +0300

    SoftUni_Python_Basics

--- Nested_Loops/Exercise/Exercise.py
+++ Nested_Loops/Exercise/Exercise.py
@@ -25,7 +25,7 @@ for number in range(first, second + 1):
     even_sum = 0
     num_to_str = str(number)
 
-    for index,value in enumerate(num_to_str):
+    for index, value in enumerate(num_to_str):
         if index % 2 == 0:
             odd_sum += int(value)
         else:
