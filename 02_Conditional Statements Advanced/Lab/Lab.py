ef552a6d2a00f37451e081c8884cc748c0bf118c
Stan <dimitrov.s.dev@gmail.com>
2024-01-03 20:49:04 +0200

    SoftUni_Python_Basics

--- 02_Conditional Statements Advanced/Lab/Lab.py
+++ 02_Conditional Statements Advanced/Lab/Lab.py
@@ -185,7 +185,7 @@ else:
 # Task 11 Fruit Shop
 
 fruit = input()
-day_of_week = (input())
+day_of_week = input()
 qty = float(input())
 
 total = 0
@@ -226,3 +226,51 @@ if total == 0:
     print("error")
 else:
     print(f"{total:.2f}")
+
+# Task 12 Trade Commissions
+
+city = input()
+sales = float(input())
+
+commission = 0
+is_invalid_input = False
+
+if city == "Sofia":
+    if 0 <= sales <= 500:
+        commission = sales * 0.05
+    elif 500 < sales <= 1000:
+        commission = sales * 0.07
+    elif 1000 < sales <= 10000:
+        commission = sales * 0.08
+    elif sales > 10000:
+        commission = sales * 0.12
+    else:
+        is_invalid_input = True
+elif city == "Varna":
+    if 0 <= sales <= 500:
+        commission = sales * 0.045
+    elif 500 < sales <= 1000:
+        commission = sales * 0.075
+    elif 1000 < sales <= 10000:
+        commission = sales * 0.1
+    elif sales > 10000:
+        commission = sales * 0.13
+    else:
+        is_invalid_input = True
+elif city == "Plovdiv":
+    if 0 <= sales <= 500:
+        commission = sales * 0.055
+    elif 500 < sales <= 1000:
+        commission = sales * 0.08
+    elif 1000 < sales <= 10000:
+        commission = sales * 0.12
+    elif sales > 10000:
+        commission = sales * 0.145
+    else:
+        is_invalid_input = True
+else:
+    is_invalid_input = True
+if is_invalid_input:
+    print("error")
+else:
+    print(f"{commission:.2f}")
