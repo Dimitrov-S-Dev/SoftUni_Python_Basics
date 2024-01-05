c0be5e9f30e4bb1459c22143d1963abbd96a5932
Stan <dimitrov.s.dev@gmail.com>
2024-01-05 14:50:19 +0200

    SoftUni_Python_Basics

--- 02_Conditional Statements Advanced/Exercise/Exercise.py
+++ 02_Conditional Statements Advanced/Exercise/Exercise.py
@@ -196,3 +196,34 @@
 #     result = f"{num_1} % {num_2} = {num_1 % num_2}"
 #
 # print(result)
+
+# Task 7 Hotel Room
+
+# month = input()
+# nights = int(input())
+#
+# studio_price = 0
+# apartment_price = 0
+# apartment = None
+# studio = None
+#
+# if month == "May" or month == "October":
+#     studio = 50
+#     apartment = 65
+#     if nights > 14:
+#         studio *= 0.7
+#     elif nights > 7:
+#         studio *= 0.95
+# elif month == "June" or month == "September":
+#     studio = 75.2
+#     apartment = 68.7
+#     if nights > 14:
+#         studio *= 0.8
+# elif month == "July" or month == "August":
+#     studio = 76
+#     apartment = 77
+#
+# if nights > 14:
+#     apartment *= 0.9
+# print(f"Apartment: {apartment * nights:.2f} lv.")
+# print(f"Studio: {studio * nights:.2f} lv.")
