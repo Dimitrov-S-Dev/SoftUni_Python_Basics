1b4198f3c39a11c002ec2a065257f17118003535
Stan <dimitrov.s.dev@gmail.com>
2024-01-05 14:41:50 +0200

    SoftUni_Python_Basics

---
+++ 02_Conditional Statements Advanced/Exercise/Exercise.py
@@ -0,0 +1,64 @@
+# Task 1 Cinema
+
+# premier_price = 12
+# normal_price = 7.5
+# discount_price = 5.0
+#
+# cinema_type = input()
+# rows = int(input())
+# cols = int(input())
+# price = None
+#
+# if cinema_type == "Premiere":
+#     price = premier_price
+# elif cinema_type == "Normal":
+#     price = normal_price
+# elif cinema_type == "Discount":
+#     price = discount_price
+#
+# result = rows * cols * price
+# print(f"{result:.2f} leva")
+
+# Task 2 Summer Outfit
+
+# celsius = int(input())
+# time_of_the_day = input()
+#
+# Outfit = None
+# Shoes = None
+#
+# if 10 <= celsius <= 18:
+#     if time_of_the_day == "Morning":
+#         Outfit = "Sweatshirt"
+#         Shoes = "Sneakers"
+#     elif time_of_the_day == "Afternoon":
+#         Outfit = "Shirt"
+#         Shoes = "Moccasins"
+#     else:
+#         Outfit = "Shirt"
+#         Shoes = "Moccasins"
+# elif 18 < celsius <= 24:
+#     if time_of_the_day == "Morning":
+#         Outfit = "Shirt"
+#         Shoes = "Moccasins"
+#     elif time_of_the_day == "Afternoon":
+#         Outfit = "T-Shirt"
+#         Shoes = "Sandals"
+#     else:
+#         Outfit = "Shirt"
+#         Shoes = "Moccasins"
+# else:
+#     if time_of_the_day == "Morning":
+#         Outfit = "T-Shirt"
+#         Shoes = "Sandals"
+#     elif time_of_the_day == "Afternoon":
+#         Outfit = "Swim Suit"
+#         Shoes = "Barefoot"
+#     else:
+#         Outfit = "Shirt"
+#         Shoes = "Moccasins"
+# print(f"It's {celsius} degrees, get your {Outfit} and {Shoes}.")
+
+
+
+
